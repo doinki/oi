4c3aa37340577191feac40c1cd3726dfdef5eac7
doinki <doinki94@gmail.com>
2024-03-31 06:29:43 +0900

    chore: Update esbuild target

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  esbuild: {
+    /**
+     * @link https://caniuse.com/bigint
+     */
+    target: ['chrome67', 'edge79', 'safari14', 'firefox68', 'opera54', 'ios14'],
+  },
   plugins: [react(), tsconfigPaths()],
 });
