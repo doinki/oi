037de267e677db58638419d6d726266f330c9445
doinki <doinki94@gmail.com>
2024-03-31 23:09:08 +0900

    feat: Add Noto Sans KR font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /**
  *  @type {import('tailwindcss').Config}
  */
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Noto Sans KR Variable', ...defaultTheme.fontFamily.sans],
+    },
+  },
 };
