61fe521efe93af99f82f093df2a3206b34c42817
doinki <doinki94@gmail.com>
2024-04-01 20:57:18 +0900

    style: Apply eslint fixes

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="sticky top-0 flex justify-between px-16 py-6">
       <strong className="text-4xl">
-        <NavLink to="/" title="Go to oi Home">
+        <NavLink title="Go to oi Home" to="/">
           oi
         </NavLink>
       </strong>
